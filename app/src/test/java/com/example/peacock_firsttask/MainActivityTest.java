af751e2be394e4fa09b4d061fed9743c1ff647b9
lokendra26 <lokendramalav1999@gmail.com>
2020-05-18 11:58:18 +0530

    Unit test for email validation

--- app/src/test/java/com/example/peacock_firsttask/MainActivityTest.java
+++ app/src/test/java/com/example/peacock_firsttask/MainActivityTest.java
@@ -8,7 +8,7 @@ public class MainActivityTest {
 
     @Test
     public void validateEmail(){
-        String input = "lokendramalav1999@gmail.com";
+        String input = "lokendra@gmail.com";
         boolean output;
         boolean expected = true;
 
